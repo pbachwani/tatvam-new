80170f12795addbe046022e88f80d5c072e90152
Prateek Bachwani <bachwani.prateek@gmail.com>
2026-07-21 20:27:08 +0530

    fixed next project

--- src/components/ui/NextProject.jsx
+++ src/components/ui/NextProject.jsx
@@ -5,14 +5,14 @@ const NextProject = ({ nextProject }) => {
     <section className="w-full h-full">
       <div className="flex">
         <div className="w-full md:w-1/2 px-4 md:p-10">
-          <img src={nextProject.cover} alt="" />
+          <img src={nextProject?.cover} alt="" />
         </div>
         <div className="w-full md:w-1/2 flex flex-col justify-start items-start font-andale gap-4 px-4 md:p-10">
           <span className="text-xl md:text-3xl font-semibold">
             Next Project
           </span>
-          <h3 className="text-2xl">{nextProject.name}</h3>
-          <p>{nextProject.brief}</p>
+          <h3 className="text-2xl">{nextProject?.name}</h3>
+          <p>{nextProject?.brief}</p>
         </div>
       </div>
     </section>
